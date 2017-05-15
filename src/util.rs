c81c13d53741775dba79e9366a21096e181822b7
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-15 15:50:46 +0200

    add more included files

--- src/util.rs
+++ src/util.rs
@@ -1,1 +1,3 @@
-const C2_HELLO_WORLD: &'static str = include_str!("../inc_files/hello_world.c2");
+pub const C2_HELLO_WORLD: &'static str = include_str!("../inc_files/hello_world.c2");
+pub const EXECUTABLE_RECIPE_STUB: &'static str = include_str!("../inc_files/executable_recipe_stub.txt");
+pub const PEBBLE_TOML: &'static str = include_str!("../inc_files/pebble.toml");
