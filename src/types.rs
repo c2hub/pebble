feb215ee161c16fb6f910e596cc0dda64d6f121d
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-17 23:13:50 +0200

    implement ToString for PebbleType

--- src/types.rs
+++ src/types.rs
@@ -4,3 +4,16 @@ pub enum PebbleType
 	SharedLib,
 	StaticLib,
 }
+
+impl ToString for PebbleType
+{
+	fn to_string(&self) -> String
+	{
+		match *self
+		{
+			PebbleType::Executable => "executable".to_string(),
+			PebbleType::SharedLib =>  "shared library".to_string(),
+			PebbleType::StaticLib => "static library".to_string()
+		}
+	}
+}
