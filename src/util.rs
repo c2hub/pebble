b86de3d6d3b062e659c0dd96dcd7b24c5f8db382
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-17 23:13:34 +0200

    include inc_files

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,12 @@
-pub const C2_HELLO_WORLD: &'static str = include_str!("../inc_files/hello_world.c2");
-pub const EXECUTABLE_RECIPE_STUB: &'static str = include_str!("../inc_files/executable_recipe_stub.txt");
 pub const PEBBLE_TOML: &'static str = include_str!("../inc_files/pebble.toml");
+
+pub const EXECUTABLE_HELLO_WORLD: &'static str = include_str!("../inc_files/executable_hello_world.c2");
+pub const EXECUTABLE_RECIPE_STUB: &'static str = include_str!("../inc_files/executable_recipe_stub.txt");
+
+pub const STATICLIB_HELLO_WORLD: &'static str  = include_str!("../inc_files/staticlib_hello_world.c2");
+pub const STATICLIB_RECIPE_STUB: &'static str  = include_str!("../inc_files/staticlib_recipe_stub.txt");
+
+pub const SHAREDLIB_HELLO_WORLD: &'static str  = include_str!("../inc_files/sharedlib_hello_world.c2");
+pub const SHAREDLIB_RECIPE_STUB: &'static str  = include_str!("../inc_files/sharedlib_recipe_stub.txt");
+
+pub const LIB_TEST: &'static str = include_str!("../inc_files/lib_test.c2");
