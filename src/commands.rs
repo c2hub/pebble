f0708ce75249aac0137afd74a8e518146297d12a
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-21 01:08:02 +0200

    implement 'pebble run', minor fixes

--- src/commands.rs
+++ src/commands.rs
@@ -430,7 +430,7 @@ pub fn scan()
 	let mut recipe = Recipe::new();
 	
 	if Recipe::find() != None
-		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path));}
+		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path.parent().unwrap()));}
 	else
 		{println!("  error: no recipe found in path"); exit(-1);}
 	
@@ -487,7 +487,7 @@ pub fn add(filename: &String)
 	let mut recipe = Recipe::new();
 	
 	if Recipe::find() != None
-		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path));}
+		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path.parent().unwrap()));}
 	else
 		{println!("  error: no recipe found in path"); exit(-1);}
 	
@@ -538,7 +538,7 @@ pub fn remove(filename: &String)
 	let mut recipe = Recipe::new();
 	
 	if Recipe::find() != None
-		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path));}
+		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path.parent().unwrap()));}
 	else
 		{println!("  error: no recipe found in path"); exit(-1);}
 	
@@ -569,7 +569,15 @@ pub fn build()
 	let mut recipe = Recipe::new();
 	
 	if Recipe::find() != None
-		{recipe.read(); let _ = set_current_dir(Path::new(&recipe.path));}
+	{
+		recipe.read_errors(true);
+		if !recipe.ok
+		{
+			println!("  error: failed to read recipe, exiting");
+			exit(-1);
+		}
+		let _ = set_current_dir(Path::new(&recipe.path.parent().unwrap()));
+	}
 	else
 		{println!("  error: no recipe found in path"); exit(-1);}
 	
@@ -637,5 +645,89 @@ pub fn build()
 		exit(-1);
 	}
 	else
-		{println!(" {}", Yellow.bold().paint("  build succeeded"));}
+		{println!("{}", Yellow.bold().paint("  build succeeded"));}
+}
+
+pub fn run(args: Vec<String>)
+{
+	let orig_cwd = match current_dir()
+	{
+		Ok(d) => d,
+		Err(_) =>
+		{
+			println!("  error: failed to get current directory path");
+			exit(-1);
+		}
+	};
+
+	build();
+
+	let mut recipe = Recipe::new();
+	recipe.read_errors(true);
+
+	// technically shouldn't occur since build() read the recipe
+	// already, but just to be sure... one never knows...
+	if !recipe.ok
+	{
+		println!("  error: failed to read recipe, exiting");
+		exit(-1);
+	}
+
+	let pebble_name = match recipe.path.parent().unwrap().file_stem()
+	{
+		Some(n) => match n.to_str()
+		{
+			Some(s) => s,
+			None =>
+			{
+				println!("  error: could not read name string");
+				exit(-1);
+			}
+		},
+		None =>
+		{
+			println!("  error: invalid project path");
+			exit(-1);
+		}
+	};	
+
+	if recipe.targets[0].kind != TargetType::Executable
+	{
+		println!("  error: pebble [{}] is a library, did you mean to use 'pebble test'?",
+			Green.bold().paint(pebble_name)
+		);
+		exit(-1);
+	}
+
+	let exe_path = match current_dir()
+	{
+		Ok(cwd) => cwd.into_os_string().into_string().unwrap()
+			+ "/output/" 
+			+ pebble_name
+			+ "/"
+			+ pebble_name,
+		Err(_) =>
+		{
+			println!("  error: failed to get current directory");
+			exit(-1);
+		}
+	};
+
+	// restore original cwd, so that pebble run can be used from anywhere within the pebble
+	if let Err(_) = set_current_dir(orig_cwd)
+	{
+		println!("  error: failed to change current directory");
+		exit(-1);
+	};
+
+	println!("  {} '{} {}'",
+		Yellow.bold().paint("running"),
+		&exe_path,
+		args.iter().fold(String::new(), |acc, ref curr| acc + " " + curr.as_ref() )
+	);
+
+	Command::new(exe_path)
+		.args(args)
+		.spawn()
+		.expect("  error: failed to launch");
 }
