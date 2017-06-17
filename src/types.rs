8f038d34b9ba26c08cdbc8f0b0005d69f48053f5
Lukáš Hozda <luk.hozda@gmail.com>
2017-06-17 16:07:18 +0200

    implement FromStr for PebbleType to allow usage of .parse()

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,40 @@
+use std::str::FromStr;
+
+use errors::PebbleError;
+
 #[derive(Clone, Copy)]
 pub enum PebbleType
 {
 	Executable,
 	SharedLib,
 	StaticLib,
+}
+
+impl FromStr for PebbleType
+{
+	type Err = PebbleError;
+	fn from_str(s: &str) -> Result<Self, Self::Err>
+	{
+		match s
+		{
+			"lib"|
+			"libstatic"|
+			"staticlib" =>
+				Ok(PebbleType::StaticLib),
+			"dynamic"|
+			"dynamiclib"|
+			"sharedlib"|
+			"libshared"|
+			"shared" =>
+				Ok(PebbleType::SharedLib),
+			"executable"|
+			"bin"|
+			"binary"|
+			"exe" =>
+				Ok(PebbleType::StaticLib),
+			x => Err(PebbleError::new(format!("invalid pebble type string '{}'", x).as_ref()))
+		}
+	}
 }
 
 impl ToString for PebbleType
