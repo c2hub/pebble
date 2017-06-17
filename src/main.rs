5e29b93e2dc3a4b489d06e3b4544b6e70ad87299
Lukáš Hozda <luk.hozda@gmail.com>
2017-06-17 16:07:48 +0200

    finally figure out how to use clap properly to achieve good results

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ extern crate hyper;
 extern crate serde;
 extern crate toml;
 extern crate sha1;
+extern crate clap;
 extern crate zip;
 
 mod commands;
@@ -21,122 +22,196 @@ mod errors;
 mod types;
 mod util;
 
-use errors::{fail, fail1};
 use types::PebbleType;
+use errors::fail1;
 use commands::*;
 
-use std::env::args;
+use clap::{App, SubCommand, Arg};
 
 fn main()
 {
-	let arguments: Vec<String> = args().collect();
+	// Note: these versions mean percentage to desired functionality, rather than actual version
+	let matches = App::new("pebble")
+		.version("0.8.15")
+		.author("Lukáš Hozda [magnusi] <luk.hozda@gmail.com>")
+		.about("Pebble is a simple package and dependency manager for C2, akin to cargo.")
+		.subcommand(SubCommand::with_name("new")
+			.about("generates a new pebble")
+			.version("1.0.0")
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("NAME")
+				.help("name of the new pebble")
+				.required(true)
+				.index(1))
+			.arg(Arg::with_name("TYPE")
+				.help("type of the pebble to produce")
+				.required(false)
+				.index(2)))
+		.subcommand(SubCommand::with_name("init")
+			.about("initializes a pebble from existing source files in a existing directory")
+			.version("1.0.0")
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("NAME")
+				.help("path to the folder where to init the pebble")
+				.required(true)
+				.index(1))
+			.arg(Arg::with_name("TYPE")
+				.help("type of the pebble to produce")
+				.required(false)
+				.index(2)))
+		.subcommand(SubCommand::with_name("scan")
+			.about("scans src/ directory for new files and starts tracking them, removes files that do not exist anymore.")
+			.version("0.9.5") // TODO update in git, too
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("add")
+			.about("starts tracking a file")
+			.version("0.9.5") // TODO add to git, too
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("FILENAME")
+				.help("the file")
+				.takes_value(true)
+				.required(true)
+				.index(1)))
+		.subcommand(SubCommand::with_name("remove")
+			.about("stops tracking a file")
+			.version("0.9.5") // TODO remove from git, too
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("FILENAME")
+				.help("the file")
+				.takes_value(true)
+				.required(true)
+				.index(1)))
+		.subcommand(SubCommand::with_name("build")
+			.about("builds the pebble, producing output in the output/ directory")
+			.version("0.6.0") // TODO check & fetch dependencies
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("run")
+			.about("builds and runs the pebble, the program is ran in CWD, only valid for executable pebbles")
+			.version("0.6.0") // TODO check & fetch dependencies
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("ARGS")
+				.help("arguments to pass to the tests")
+				.min_values(1)
+				.required(false)))
+		.subcommand(SubCommand::with_name("test")
+			.about("builds and runs test of a pebble, only valid for library pebbles")
+			.version("0.6.0") // TODO check & fetch dependencies
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("ARGS")
+				.help("arguments to pass to the tests")
+				.min_values(1)
+				.required(false)))
+		.subcommand(SubCommand::with_name("install")
+			.about("runs install instructions of a pebble")
+			.version("0.9.0") // Also allow downloading and installing of foreign pebbles, not just this one
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("uninstall")
+			.about("runs uninstall instructions of a pebble")
+			.version("0.9.0") // Also allow removal of foreign pebbles
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("update")
+			.about("the fate of this command is unclear as of yet")
+			.version("0.1.0") // Implement something meaningful. Either update dependencies or check if there is an update for a pebble
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("find")
+			.about("tries to find a pebble in the pebble index, if found, prints the latest version")
+			.version("1.0.0")
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("NAME")
+				.help("the name of the pebble to find")
+				.required(true)
+				.index(1)))
+		.subcommand(SubCommand::with_name("register")
+			.about("register with pebble")
+			.version("1.0.0")
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("USERNAME")
+				.help("the username to register with")
+				.required(true)
+				.index(1))
+			.arg(Arg::with_name("PASSWORD")
+				.help("your password, there are no restrictions, but please, 1234 is a bad password")
+				.required(true)
+				.index(2)))
+		.subcommand(SubCommand::with_name("login")
+			.about("login with pebble, session only last until you restart your PC")
+			.version("1.0.0")
+			.author("Lukáš Hozda")
+			.arg(Arg::with_name("USERNAME")
+				.help("the username to login with")
+				.required(true)
+				.value_name("USERNAME")
+				.takes_value(true)
+				.index(1))
+			.arg(Arg::with_name("PASSWORD")
+				.help("your password")
+				.value_name("PASSOWRD")
+				.required(true)
+				.takes_value(true)
+				.index(2)))
+		.subcommand(SubCommand::with_name("package")
+			.about("package the pebble (confirm ability to be built, zip up)")
+			.version("1.0.0")
+			.author("Lukáš Hozda"))
+		.subcommand(SubCommand::with_name("upload")
+			.about("upload a pebble to the pebble index, uploads a pebble as a whole, not like a library")
+			.version("0.9.0") // Chop up bytes and send it as parts, not like one huge packet. That will also allow pebbles bigger than 60kb
+			.author("Lukáš Hozda"))
+		.get_matches();
 
-	if arguments.len() < 2
-		{help(&String::new());}
-
-	match arguments[1].as_ref()
+	match matches.subcommand()
 	{
-		"new" =>
+		("new", Some(m)) =>
 		{
-			match arguments.len()
+			if let Some(s) = m.value_of("TYPE")
 			{
-				3 => new_pebble(&arguments[2], PebbleType::Executable),
-				4 => match arguments[3].as_ref()
+				if let Ok(t) = s.parse()
 				{
-					"lib"|
-					"libstatic"|
-					"staticlib" =>
-						new_pebble(&arguments[2], PebbleType::StaticLib),
-					"dynamic"|
-					"dynamiclib"|
-					"sharedlib"|
-					"libshared"|
-					"shared" =>
-						new_pebble(&arguments[2], PebbleType::SharedLib),
-					"executable"|
-					"bin"|
-					"binary"|
-					"exe" =>
-						new_pebble(&arguments[2], PebbleType::Executable),
-					x => fail1("unknown pebble type '{}'", x, 1)
-				},
-				_ => fail("usage: pebble new <name> <type>", 1)
+					new_pebble(m.value_of("NAME").unwrap(), t);
+				}
+				else
+				{
+					new_pebble(m.value_of("NAME").unwrap(), PebbleType::Executable);
+				}
 			}
-		}
-		"init" => match arguments.len()
+		},
+		("init", Some(m)) =>
 		{
-			3 => init_pebble(&arguments[2], PebbleType::Executable),
-			4 => match arguments[3].as_ref()
+			if let Some(s) = m.value_of("TYPE")
 			{
-				"lib"|
-				"libstatic"|
-				"staticlib"|
-				"static"|
-				"slib" =>
-					init_pebble(&arguments[2], PebbleType::StaticLib),
-				"dynamic"|
-				"dynamiclib"|
-				"sharedlib"|
-				"libshared"|
-				"shared"|
-				"dlib" =>
-					init_pebble(&arguments[2], PebbleType::SharedLib),
-				"executable"|
-				"bin"|
-				"binary"|
-				"exe" =>
-					init_pebble(&arguments[2], PebbleType::Executable),
-				x => fail1("unknown pebble type '{}'", x, 1)
-			},
-			_ => fail("usage: pebble new <name> <type>", 1)
+				match s.parse()
+				{
+					Ok(t) => init_pebble(m.value_of("NAME").unwrap(), t),
+					Err(e) => fail1("{}", e, -1)
+				}
+			}
+			else
+			{
+				init_pebble(m.value_of("NAME").unwrap(), PebbleType::Executable);
+			}
 		},
-		"scan" => match arguments.len()
-		{
-			2 => scan(),
-			_ => fail("usage: pebble scan #what else?", 1)
-		},
-		"add" => match arguments.len()
-		{
-			3 => add(&arguments[2]),
-			_ => fail("usage: pebble add 'filename'", 1)
-		},
-		"remove" | "del" => match arguments.len()
-		{
-			3 => remove(&arguments[2]),
-			_ => fail("usage: pebble remove 'filename'", 1)
-		},
-		"build" => match arguments.len()
-		{
-			2 => build(),
-			_ => fail("usage: pebble build", 1)
-		},
-		"help" => match arguments.len()
-		{
-			2 => help(&String::new()),
-			_ => help(&arguments[2].to_string()),
-		},
-		"run" => run(arguments.clone().into_iter().skip(2).collect()),
-		"test" => test(arguments.clone().into_iter().skip(2).collect()),
-		"install" => install(),
-		"uninstall" => uninstall(),
-		"update" => update(),
-		"find" => match arguments.len()
-		{
-			3 => find(&arguments[2].to_string()),
-			_ => help(&String::new()),
-		},
-		"register" => match arguments.len()
-		{
-			4 => register(&arguments[2].to_string(), &arguments[3].to_string()),
-			_ => help(&String::new())
-		},
-		"login" => match arguments.len()
-		{
-			4 => login(&arguments[2].to_string(), &arguments[3].to_string()),
-			_ => help(&String::new())
-		},
-		"package" => { let _ = package(); }
-		"upload" => upload(),
-		x => fail1("unknown operation: '{}'", x, 1)
+		("scan",            _) => scan(),
+		("add",       Some(m)) => add(m.value_of("FILENAME").unwrap()),
+		("remove",    Some(m)) => remove(m.value_of("FILENAME").unwrap()),
+		("build",           _) => build(),
+		("run",       Some(m)) => run(m.values_of_lossy("ARGS").unwrap_or(Vec::new())),
+		("test",      Some(m)) => test(m.values_of_lossy("ARGS").unwrap_or(Vec::new())),
+		("install",         _) => install(),
+		("uninstall",       _) => uninstall(),
+		("update",          _) => update(),
+		("find",      Some(m)) => find(m.value_of("NAME").unwrap()),
+		("register",  Some(m)) => register(
+			m.value_of("USERNAME")
+				.unwrap(),
+			m.value_of("PASSWORD").unwrap()
+		),
+		("login",     Some(m)) => login(
+			m.value_of("USERNAME")
+				.unwrap(),
+			m.value_of("PASSWORD").unwrap()
+		),
+		("package", _) => {let _ = package();},
+		("upload",  _) => upload(),
+		_ => unreachable!()
 	}
 }
