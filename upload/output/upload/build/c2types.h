0200072dbc79f1b2a01c912a72732af19c8c8be6
Lukáš Hozda <luk.hozda@gmail.com>
2018-07-29 19:32:20 +0200

    minor

---
+++ upload/output/upload/build/c2types.h
@@ -0,0 +1,23 @@
+#ifndef C2_TYPES_H
+#define C2_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NULL ((void*)0)
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long int64_t;
+typedef unsigned long uint64_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
